e310a5145a037f08fa60ed106fe7b76cd99d12b0
Percis Brenda Mukyala <persismukyala@gmail.com>
2020-11-20 13:10:39 +0300

    Grading system modified

--- gradingsystem/grade.js
+++ gradingsystem/grade.js
@@ -1,4 +1,4 @@
-let result=input;
+/*let result=input;
 if(result>=80&&===100){
     grade='A'
 }elseif(result <=79&&>=70){
@@ -14,7 +14,36 @@ if(result>=80&&===100){
     grade="O"
 }elseif(result<40&&>=0){
     grade="F"
-}else{"invalid result"}
+}else{"invalid result"}*/
+$(".remove").click(function(){
+    $("input-score").val("");
+    $(".display").text("");
+});
+
+
+let result = $(".inputResult").val();
+let grade;
+$(".input").click(function () {
+    if (result >= 90 && result <= 100 ) {
+        grade = "A"
+        $(".display").text(`Grade ${grade}.`);
+    } else if (result >= 80 && result <= 89 ) {
+        grade = "B"
+        $(".display").text(`Grade ${grade}.`);
+    } else if (result >= 70 && result <= 79 ) {
+        grade = "C"
+        $(".display").text(`Grade ${grade}.`);
+    } else if (result >= 60 && result <= 69 ) {
+        grade = "D"
+        $(".display").text(`Grade ${grade}.`);
+    } else if (result >= 50 && result <= 59 ) {
+        $(".display").text(`Grade E.`);
+    } else if (result >= 0 && result <= 49 ) {
+        $(".display").text('Grade F');
+    } else {
+        $(".display").text("Invalid input");
+    }
+});
     
 
     
